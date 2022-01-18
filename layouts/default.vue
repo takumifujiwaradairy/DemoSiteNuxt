5714b6a475da431a1b44c9d303935c7333345316
藤原琢巳 <fujiwaratakumi@fujiwaratakuminoMacBook-Pro.local>
2022-01-18 17:56:59 +0900

    Headerの作成

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <TheHeader/>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+  import TheHeader from './TheHeader.vue'
+  
+export default {
+  components:{
+    TheHeader,
+  }
+}
+</script>
