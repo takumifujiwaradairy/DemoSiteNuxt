c7ceda0cd9ac88ee83de452ef8772024f0a4d662
藤原琢巳 <fujiwaratakumi@fujiwaratakuminoMacBook-Pro.local>
2022-01-19 16:06:53 +0900

    StoreFakeFata

--- components/pages/SubmitForm.vue
+++ components/pages/SubmitForm.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-    <input type="text" name="">
-    <textarea></textarea>
+    <input type="text" name="title">
+    <textarea name="content"></textarea>
+    <div>
+      <button>save</button> 
+      <button>find</button> 
     </div>
+  </div>
 </template>
     
 <script>
--- store/index.js
+++ store/index.js
@@ -3,6 +3,16 @@ import Vuex from 'vuex';
 const createStore = () => {
   return new Vuex.Store({
     state:{
+      articles:[
+        {title: 'おすすめの化粧水10選',
+        content: 'hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge',
+        created: '2022-1-18 16:00'
+      },
+        {title: '今海外で流行中の美容法',
+        content: 'hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge',
+        created: '2022-1-20 16:00'
+      }
+      ]
     },
     mutations:{
     },
